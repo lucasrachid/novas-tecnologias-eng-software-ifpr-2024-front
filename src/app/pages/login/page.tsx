a82a8357bcb1f31a859f83b8c2ee08ca166d7c5d
Lucas Rachid Martins <lucasrachid@hotmail.com>
2024-07-08 20:47:54 -0300

    login - component.

--- src/app/pages/login/page.tsx
+++ src/app/pages/login/page.tsx
@@ -1,7 +1,58 @@
+"use client"
+import { useState } from 'react';
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+import "./page.scss";
+import HttpService from '@/app/services/httpService';
+
 export default function Login() {
+
+    const httpService = new HttpService();
+
+    const [loginInfo, setLoginInfo] = useState({
+        username: '',
+        password: ''
+    });
+
+    const handleChange = (e: any) => {
+        const { id, value } = e.target;
+        setLoginInfo(prevState => ({
+            ...prevState,
+            [id]: value
+        }));
+    };
+
+    const authentication = async () => {
+        const authBody = { username: loginInfo.username, password: loginInfo.password };
+        const response = await httpService.post('/api/token/', authBody);
+        console.log(response);
+    };
+
     return (
-        <main className="flex min-h-screen flex-col items-center justify-between p-24">
-            <div>Login</div>
+        <main className="">
+            <div className="flex flex-col h-[200px] justify-around">
+                <TextField
+                    id="username"
+                    label="Login"
+                    variant="outlined"
+                    value={loginInfo.username}
+                    onChange={handleChange}
+                />
+                <TextField
+                    id="password"
+                    label="Password"
+                    variant="outlined"
+                    type="password"
+                    value={loginInfo.password}
+                    onChange={handleChange}
+                />
+                <Button
+                    variant="contained"
+                    size="large"
+                    onClick={authentication}>
+                    Acessar
+                </Button>
+            </div>
         </main>
     );
 }
--- src/environments/environments.ts
+++ src/environments/environments.ts
@@ -1,3 +1,3 @@
 export const ENV = {
-    apiUrl: 'http://localhost:8000/api/v1',
+    apiUrl: 'http://localhost:8000',
 };
